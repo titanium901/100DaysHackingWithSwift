853f919530308a376729c101df2eb01e683f6d10
Iurii Popov <30910230+titanium901@users.noreply.github.com>
2019-07-05 10:59:22 +0300

    Add files via upload

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,158 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Yury Popov on 04/07/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var gameScore: SKLabelNode!
+    var finalScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var slots = [WhackSlot]()
+    var popupTime = 0.85
+    var numRounds = 0
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
+        
+        rain()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        
+        
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+           
+                        
+            if node.name == "charFriend" {
+                // they shouldn't have whacked this penguin
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion:false))
+            } else if node.name == "charEnemy" {
+                // they should have whacked this one
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                score += 1
+                smoke(box: whackSlot)
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion:false))
+            }
+        }
+    }
+    
+    func createSlot(at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
+    }
+    
+    func createEnemy() {
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            showFinalScore()
+            run(SKAction.playSoundFileNamed("gameOver.m4a", waitForCompletion:false))
+            
+            return
+        }
+        
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
+    }
+    
+    func showFinalScore() {
+        finalScore = SKLabelNode(fontNamed: "Chalkduster")
+        finalScore.text = "Final Score: \(score)"
+        finalScore.position = CGPoint(x: 512, y: 470)
+        finalScore.fontColor = UIColor.black
+       
+        finalScore.fontSize = 48
+        addChild(finalScore)
+    }
+    
+    func smoke(box: SKNode) {
+        if let smokeParticles = SKEmitterNode(fileNamed: "SmokeParticles") {
+            smokeParticles.position = box.position
+            smokeParticles.zPosition = 1
+            smokeParticles.position.y += 15
+            addChild(smokeParticles)
+        }
+       
+    }
+    
+    func rain() {
+        if let rainParticle = SKEmitterNode(fileNamed: "RainParticle") {
+            rainParticle.position = CGPoint(x: 1024, y: 768)
+            rainParticle.zPosition = 1
+            rainParticle.position.y += 15
+            addChild(rainParticle)
+        }
+    }
+    
+    
+    
+
+}
---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,85 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by Yury Popov on 04/07/2019.
+//  Copyright © 2019 Yury Popov. All rights reserved.
+//
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        mud(box: charNode)
+        isVisible = false
+    }
+    
+    func hit() {
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
+        charNode.run(SKAction.sequence([delay, hide, notVisible]))
+    }
+    
+    func mud(box: SKNode) {
+        if let mudParticle = SKEmitterNode(fileNamed: "Mud") {
+            mudParticle.position = box.position
+            mudParticle.zPosition = 1
+            
+            addChild(mudParticle)
+        }
+    }
+}
