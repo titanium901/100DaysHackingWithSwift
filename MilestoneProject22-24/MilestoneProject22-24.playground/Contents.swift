0af4e05c4e694275d4fc1a85356dcd2044d94471
Iurii Popov <30910230+titanium901@users.noreply.github.com>
2019-07-26 17:27:46 +0300

    Add files via upload

---
+++ MilestoneProject22-24/MilestoneProject22-24.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+import PlaygroundSupport
+
+// SHOW ASSISTANT EDITOR!
+// Setup Live View
+let container = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 250))
+container.backgroundColor = .darkGray
+PlaygroundPage.current.liveView = container
+
+// Challenge 1: Extend UIView so that it has a bounceOut(duration:) method that uses animation to scale its size down to 0.0001 over a specified number of seconds.
+extension UIView {
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
+
+let view = UIView(frame: CGRect(x: 150, y: 25, width: 200, height: 200))
+view.layer.cornerRadius = 100
+view.backgroundColor = .red
+container.addSubview(view)
+view.bounceOut(duration: 1.0)
+
+// Challenge 2: Extend Int with a times() method that runs a closure as many times as the number is high. For example, 5.times { print("Hello!") } will print “Hello” five times.
+extension Int {
+    func times(action: () -> Void) {
+        guard self > 0 else { return }
+        for _ in 0 ..< self {
+            action()
+        }
+    }
+}
+
+5.times {
+    print("Hello!")
+}
+
+// Challange 3: Extend Array so that it has a mutating remove(item:) method. If the item exists more than once, it should remove only the first instance it finds. Tip: you will need to add the Comparable constraint to make this work!
+extension Array where Element: Comparable {
+    mutating func removeItem(item: Element) {
+        if let index = self.firstIndex(of: item) {
+            self.remove(at: index)
+        }
+    }
+}
+
+var list = ["bird", "cat", "dog", "dog", "fish"]
+list.removeItem(item: "dog")
+print(list)
